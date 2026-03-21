3c1c80618142f975186c61aa6b2a22ea4d992d37
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2026-03-22 00:27:46 +0100

    Add TweakParser

---
+++ Parser/TweakParser.cs
@@ -0,0 +1,30 @@
+﻿using TweakLib.Models.Actions;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace TweakLib.Parser
+{
+    public static class TweakParser
+    {
+        public static IDeserializer Deserializer { get; } = new DeserializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .WithTagMapping("!cmd:", typeof(CmdAction))
+            .WithTagMapping("!powerShell:", typeof(PowerShellAction))
+            .WithTagMapping("!registryValue:", typeof(RegistryValueAction))
+            .WithTagMapping("!run:", typeof(RunAction))
+            .WithTagMapping("!scheduledTask:", typeof(ScheduledTaskAction))
+            .WithTagMapping("!service:", typeof(ServiceAction))
+            .Build();
+
+        public static ISerializer Serializer { get; } = new SerializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .WithTagMapping("!cmd:", typeof(CmdAction))
+            .WithTagMapping("!powerShell:", typeof(PowerShellAction))
+            .WithTagMapping("!registryValue:", typeof(RegistryValueAction))
+            .WithTagMapping("!run:", typeof(RunAction))
+            .WithTagMapping("!scheduledTask:", typeof(ScheduledTaskAction))
+            .WithTagMapping("!service:", typeof(ServiceAction))
+            .EnsureRoundtrip()
+            .Build();
+    }
+}
