a47aa3ce91c8a8868c119fea4e1f4f62147477ba
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2026-03-27 19:14:46 +0100

    Fix registry value deletion

--- Actions/RegistryValueAction.cs
+++ Actions/RegistryValueAction.cs
@@ -116,7 +116,7 @@ namespace TweakLib.Actions
             string arguments = Operation switch
             {
                 RegistryValueOperation.Modify => $"add \"{Path}\" /v \"{Value}\" /t {type} /d \"{Data}\" /f",
-                RegistryValueOperation.Delete => $"delete \"{Path}\" /f",
+                RegistryValueOperation.Delete => $"delete \"{Path}\" /v \"{Value}\" /f",
                 _ => throw new NotImplementedException()
             };
 
